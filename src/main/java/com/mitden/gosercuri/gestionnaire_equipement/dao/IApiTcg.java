a20e4645497d08d86fd52c839db300f1665707c2
samuel vilrus <samuel.tatel-vilrus.ext@hotelbb.com>
2020-07-11 15:27:40 +0200

    Suppression de fichier

--- src/main/java/com/mitden/gosercuri/gestionnaire_equipement/dao/IApiTcg.java
+++
@@ -1,9 +0,0 @@
-package com.mitden.gosercuri.gestionnaire_equipement.dao;
-
-import com.mitden.gosercuri.gestionnaire_equipement.dao.model.User;
-
-import java.util.List;
-
-public interface IApiTcg {
-    public List<User> getUsers() throws Exception;
-}
